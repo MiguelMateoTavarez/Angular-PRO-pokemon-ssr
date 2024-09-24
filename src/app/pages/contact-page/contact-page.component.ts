dc089ac3e692e423b9041246628359dd6930ce61
Miguel Mateo <miguelmateot94@gmail.com>
2024-09-24 15:31:05 -0400

    feat: implement SEO friendly

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'contact-page',
@@ -7,4 +8,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactPageComponent { }
+export default class ContactPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact');
+    this.meta.updateTag({ name: 'description', content: 'Contact page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Contact page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Miguel Mateo Angular PRO' });
+  }
+}
